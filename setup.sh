8f5441d69ce052544b64da3b58e3b61850e5abd6
Paul Baumstark <41694868+pbmstrk@users.noreply.github.com>
2023-05-18 22:08:30 +0100

    update setup script

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ rm nytdata.duckdb
 source .venv/bin/activate
 load-archive-data 1997 2023
 
-./duckdb nytdata.duckdb < sql/setup.sql
+duckdb nytdata.duckdb < sql/setup.sql
 dbt run --project-dir sql/
-./duckdb nytdata.duckdb < sql/export.sql 
+duckdb nytdata.duckdb < sql/export.sql 
