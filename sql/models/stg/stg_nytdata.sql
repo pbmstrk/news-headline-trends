5b49b85cce8c4fc3f4a40b4f0ec1fca1a8f6d7c1
Paul Baumstark <41694868+pbmstrk@users.noreply.github.com>
2023-05-25 11:59:29 +0100

    use strftime

--- sql/models/stg/stg_nytdata.sql
+++ sql/models/stg/stg_nytdata.sql
@@ -12,6 +12,5 @@ with stg_data as (select
     from {{ source('raw_data', 'nytdata') }}
 )
 
-select *, 
-concat(extract('year' from pub_date), '-', right('00' || cast(extract('month' from pub_date) as varchar), 2)) as year_month from stg_data
+select *, strftime(pub_date, '%Y-%m') as year_month from stg_data
 where extract('year' from pub_date) = cast(filename[7:10] as int)
