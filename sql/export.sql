a6b2feb6fe0205863376858e020e73f089b995b3
Paul Baumstark <41694868+pbmstrk@users.noreply.github.com>
2023-06-08 21:56:35 +0100

    export only csv files

--- sql/export.sql
+++ sql/export.sql
@@ -1,5 +1,2 @@
-copy monthly_content_counts to 'data/monthly_content_counts.parquet' (CODEC 'GZIP');
-copy word_headlines to 'data/word_headlines.parquet';
-
 copy monthly_content_counts to 'data/monthly_content_counts.csv';
 copy word_headlines to 'data/word_headlines.csv';
