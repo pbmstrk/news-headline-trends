5d3ba970affd6d0bc09c4d9850633fd2196ddb89
Paul Baumstark <41694868+pbmstrk@users.noreply.github.com>
2023-09-28 19:52:01 +0100

    add initial frontend

---
+++ frontend/src/KeywordInput.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+function KeywordInput({ keywords, setKeywords }) {
+    const [inputValue, setInputValue] = useState('');
+    const [warning, setWarning] = useState(''); 
+
+    // Handle any change to reset any existing warnings.
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value); // need to set input value, otherwise it won't change
+        setWarning(''); 
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter' && inputValue) {
+            if (inputValue.includes(' ')) {
+                setWarning('Multi-word keywords are not supported'); // Set warning for multi-word keyword
+                return;
+            }
+            if (!keywords.includes(inputValue)) {
+                setKeywords([...keywords, inputValue]);
+            }
+            setInputValue('');
+        } else if (event.key === 'Backspace' && !inputValue && keywords.length > 0) {
+            // Remove the last keyword when backspace is pressed and the input is empty
+            setKeywords(keywords.slice(0, -1));
+        }
+    };
+
+    const handleRemoveKeyword = (keywordToRemove) => {
+        setKeywords(keywords.filter(keyword => keyword !== keywordToRemove));
+    };
+
+    return (
+        <div>
+            {warning && <div className="bg-red-100 text-red-500 mt-2 w-fit rounded p-1">{warning}</div>} {/* Display the warning if present */}
+            <div className="flex items-center p-1 border rounded mt-2 max-w-xl">
+                {keywords.map(keyword => (
+                    <span key={keyword} className="border border-blue-400 text-blue-500 mr-2 rounded pl-1 bg-blue-100">
+                        {keyword}
+                        <button className="mx-2 rounded-full" onClick={() => handleRemoveKeyword(keyword)}>x</button>
+                    </span>
+                ))}
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
+                    placeholder="Enter search terms..."
+                    className="flex-1 outline-none"
+                />
+            </div>
+        </div>
+    );
+}
+
+export default KeywordInput;
