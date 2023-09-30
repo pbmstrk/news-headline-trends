17b7868c26866435abbdc216f164bb5e900b3e7a
Paul Baumstark <41694868+pbmstrk@users.noreply.github.com>
2023-09-30 21:07:48 +0200

    add logging to clojure

--- nytdata-clj/src/nytdata/main.clj
+++ nytdata-clj/src/nytdata/main.clj
@@ -6,7 +6,8 @@
             [next.jdbc :as jdbc]
             [next.jdbc.sql :as sql]
             [nytdata.utils.date :as date-utils]
-            [nytdata.utils.db :as db-utils]))
+            [nytdata.utils.db :as db-utils]
+            [taoensso.timbre :as log]))
 
 (defn construct-nyt-api-url
   "Create the URL for the http request based on year-month string."
@@ -42,10 +43,10 @@
     (jdbc/execute-batch! ds query (map extract-metadata docs) {})))
 
 (defn process-month [ds ym api-key]
-  (println (str "Fetching data for month: " ym))
+  (log/info "Fetching data for month: " ym)
   (let [docs (fetch-nyt-data-for-month ym api-key)
         num-docs (count docs)]
-    (println (str "\tNumber of records: " num-docs))
+    (log/info "Number of records: " num-docs)
     (insert-headlines! ds docs)
     (sql/insert! ds :process_log {:year_month ym :num num-docs})))
 
@@ -60,11 +61,11 @@
         ds (db-utils/get-datasource-from-env)
         latest-ym (db-utils/get-latest-processed-month ds default-start)
         ym-seq (date-utils/year-month-sequence latest-ym)]
-    (println (str "Processing months: " ym-seq))
+    (log/info "Processing months: " ym-seq)
     (doseq [ym ym-seq]
       (process-month ds ym api-key)
       (Thread/sleep 12000))
     (when (seq ym-seq)
-      (println "Refreshing materialized views")
+      (log/info "Refreshing materialized views")
       (jdbc/execute! ds ["refresh materialized view monthly_content_counts_vw"]))))
 
