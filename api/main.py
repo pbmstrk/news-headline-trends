702bcb3bfcdd85cfa1e12c1c5561df3918c46d26
Paul Baumstark <41694868+pbmstrk@users.noreply.github.com>
2023-10-05 22:43:46 +0100

    use connection in request instead of engine

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from sqlalchemy import create_engine, text, Engine
+from sqlalchemy import create_engine, text, Connection, Engine
 from fastapi import FastAPI, Depends, Request
 from fastapi.middleware.cors import CORSMiddleware
 from slowapi import Limiter, _rate_limit_exceeded_handler
@@ -43,11 +43,10 @@ def fetch_database_url():
     return database_url
 
 
-def execute_query(engine, sql, **query_params):
+def execute_query(connection, sql, **query_params):
     """Executes an SQL query and returns the result as a DataFrame."""
     query = text(sql).bindparams(**query_params)
-    with engine.connect() as con:
-        return pd.read_sql(query, con)
+    return pd.read_sql(query, connection)
     
 def create_db_engine() -> Engine:
     db_url = fetch_database_url()
@@ -67,24 +66,30 @@ app.add_middleware(
 )
 
 
+engine = create_db_engine()
+
+def get_connection():
+    with engine.connect() as connection:
+        yield connection
+
 @app.get("/samples")
 @limiter.limit("300/minute")
-def get_sample(request: Request, year_month: str, keyword: str, engine: Engine = Depends(create_db_engine)):
+def get_sample(request: Request, year_month: str, keyword: str, connection: Connection = Depends(get_connection)):
     """Retrieve a sample of headlines from the database based on a given year-month and keyword.
 
     The function searches the headlines database for records that match the specified `year_month` 
     and contain the `keyword`. It then randomly selects a sample of the results, with a maximum 
     of 5 records. If there are fewer than 5 records, it returns all of them."""
     result = execute_query(
-        engine, SQL_HEADLINES_QUERY, keyword=keyword, year_month=year_month
+        connection, SQL_HEADLINES_QUERY, keyword=keyword, year_month=year_month
     )
     sample = result.sample(min(5, result.shape[0]))
 
     return sample.to_dict("records")
 
-def query_occurrences(keyword: str, engine: Engine) -> pd.DataFrame:
+def query_occurrences(keyword: str, connection: Connection) -> pd.DataFrame:
     """Query occurrences for a given keyword and add keyword as a column."""
-    result = execute_query(engine, SQL_FULL_TEXT_SEARCH, word=keyword)
+    result = execute_query(connection, SQL_FULL_TEXT_SEARCH, word=keyword)
     return (result, keyword)
 
 def fill_missing_months(df: pd.DataFrame, date_range: list[str]) -> pd.DataFrame:
@@ -99,9 +104,9 @@ def fill_missing_months(df: pd.DataFrame, date_range: list[str]) -> pd.DataFrame
 
 @app.get("/occurrences")
 @limiter.limit("300/minute")
-def get_occurences(request: Request, keywords: str, engine: Engine = Depends(create_db_engine)):
+def get_occurences(request: Request, keywords: str, connection: Connection = Depends(get_connection)):
     tags = keywords.split(",")
-    df_list = [query_occurrences(tag, engine) for tag in tags]
+    df_list = [query_occurrences(tag, connection) for tag in tags]
     result_df = pd.concat([df[0] for df in df_list])
 
     # Create a full date range for the combined result
@@ -121,5 +126,5 @@ def get_occurences(request: Request, keywords: str, engine: Engine = Depends(cre
         for date, count in zip(df["year_month"], df["num_headlines"]):
             data.append({"x": date, "y": count})
         result.append({"id": keyword, "data": data})
-    
+
     return result
