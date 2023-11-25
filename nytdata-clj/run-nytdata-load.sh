c934f0951f02eb97f5da9cd05fcf0e86d272d270
Paul Baumstark <41694868+pbmstrk@users.noreply.github.com>
2023-11-24 17:41:24 -0800

    add command to build docker image to load script

--- nytdata-clj/run-nytdata-load.sh
+++ nytdata-clj/run-nytdata-load.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-# Load environment variables from .env file
+echo "Loading environment variables"
 set -a
 source ../.env
 set +a
+
+# Build Docker Image
+echo "Building Docker image..."
+docker build -t nytdata-load .
+
+# Check if Docker build was successful
+if [ $? -ne 0 ]; then
+    echo "Docker build failed, exiting."
+    exit 1
+fi
+echo "Docker image built successfully."
 
 # Construct the JDBC URL
 JDBC_DATABASE_URL="jdbc:postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db:5432/nytdata"
